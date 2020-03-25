4a57db08db6a30f851a06ddd6b3b55f8ead4336c
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-03-25 03:23:12 +0200

    Add GraphVisualizer GUI class
    This is the container class which spawns the window and contain all the
    tabs

---
+++ GraphVisualizerGUI.py
@@ -0,0 +1,26 @@
+import tkinter as tk
+import tkinter.ttk as ttk
+from InputsTab import *
+
+class GraphVisualizerGUI:
+    def __init__(self, callback):
+        # Initalize main window
+        self.masterWindow = tk.Tk(className="Graph visualizer")
+        self.callerCallback = callback
+
+        # Add notebook (tabs manager)
+        self.masterNotebook = ttk.Notebook(self.masterWindow)
+        self.masterNotebook.pack(expand=True)
+
+        # Create inputs tab
+        self.inputsTab = InputsTab(self.masterNotebook, self._callbackInput)
+
+        self.masterWindow.mainloop()
+    
+    def _callbackInput(self, adjList):
+        # adjList is a dict with int key and list value
+        # what more does a caller need ? maybe return something ?
+        # return a tuple, [0] is an image path for visuals
+        self.callerCallback(adjList)
+        pass
+    
